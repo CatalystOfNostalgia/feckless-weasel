9b69026748a22109f577935ce0e6c1ce52284b05
Christian Gunderman <gundermanc@gmail.com>
2015-09-18 20:39:27 -0400

    Directory structure

---
+++ service/src/test/java/com/fecklessweasel/service/test_Example.java
@@ -0,0 +1,19 @@
+package com.fecklessweasel.service;
+
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.Ignore;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+public class test_Example {
+
+    @Test
+    public void test_ExampleTest() {
+        // TODO: Write tests.
+        fail();
+    }
+}
