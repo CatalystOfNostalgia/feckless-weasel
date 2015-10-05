6bc19fca08acef1da313afca9aa4013d340a8b1c
Hayden Schmackpfeffer <hbs25@case.edu>
2015-10-04 20:33:20 -0400

    added tests for FileMetadata - incomplete

---
+++ service/src/test/java/com/fecklessweasel/service/objectmodel/test_FileMetadata.java
@@ -0,0 +1,47 @@
+package com.fecklessweasel.service.objectmodel;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.Test;
+import org.junit.Ignore;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.SQLIntegrityConstraintViolationException;
+import java.sql.Statement;
+import java.util.Date;
+
+import com.fecklessweasel.service.objectmodel.ServiceException;
+import com.fecklessweasel.service.objectmodel.ServiceStatus;
+
+/**
+  * Unit tests for the object model FileMetadata
+  */
+public class test_FileMetadata {
+  private Connection mockConnection;
+
+  @Before
+  public void setup() {
+      this.mockConnection = mock(Connection.class);
+  }
+
+  @Test
+  public void test_create_NullSQL_Exception() throws Exception {
+      try {
+          FileMetadata.create(null,
+                      "user",
+                      "course",
+                      "university",
+                      new Date());
+      } catch (ServiceException ex) {
+          assertEquals(ServiceStatus.NO_SQL, ex.status);
+          return;
+      }
+
+      fail("No service exception thrown");
+  }
+}
