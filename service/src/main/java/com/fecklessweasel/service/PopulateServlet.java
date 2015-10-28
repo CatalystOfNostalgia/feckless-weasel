b76259851248901a7cd200659ff8c0c4bb7469df
Hayden Schmackpfeffer <hbs25@case.edu>
2015-10-28 00:10:17 -0400

    page at /servlet/populate will automatically populate the DB with test data and show what was added on screen

--- service/src/main/java/com/fecklessweasel/service/PopulateServlet.java
+++ service/src/main/java/com/fecklessweasel/service/PopulateServlet.java
@@ -6,8 +6,10 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.RequestDispatcher;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import com.fecklessweasel.service.datatier.SQLSource;
 import com.fecklessweasel.service.datatier.SQLInteractionInterface;
@@ -26,26 +28,37 @@ import com.fecklessweasel.service.objectmodel.FileMetadata;
  * @author Hayden Schmackpfeffer
  */
 @WebServlet("/servlet/populate")
-public final class UserServlet extends HttpServlet {
+public final class PopulateServlet extends HttpServlet {
+    //Test User data
+    public static final String[] usernames = {"hschmack", "person1",
+                          "SteveJobs27", "Drake13"};
+    public static final String[] passwords = {"password1", "pass_word2",
+                          "PassWord3", "P455W0RD4"};
+    public static final String[] emails = {"hayden@case.edu", "generic@gmail.com",
+                          "jobs@apple.com", "hotlinebling@yahoo.com"};
+    //Test University data
+    public static final String[] univ_name = {"Case Western Reserve University", "University of Rochester",
+                                                "University of Michigan", "Boston University"};
+    public static final String[] univ_acron = {"CWRU", "UR", "UM", "BU"};
+    public static final String[] univ_city = {"Cleveland", "Rochester", "Ann arbor", "Boston"};
+    public static final String[] univ_state= {"OH", "NY", "MI", "MA"};
+
+    //Test Dept data
+    public static final String[] dept_names = {"Electrical Engineering and Computer Science" , "History",
+                                              "Philosophy", "Physics"};
+    public static final String[] dept_acro = {"EECS", "HIST", "PHIL", "PHYS"};
+
+    //Test Course data
+    public static final int[] course_nums = {397, 233, 390, 201, 102, 300};
 
 
-    /**
-     * Handles post requests to this servlet and address.
-     * @param request The HTTP request containing the form data for
-     * creating a user.
-     * @param response The HTTP response containing the redirect to
-     * a UI page.
-     * @throws ServletException Thrown if the service encounters an
-     * error.
-     * @throws IOException Thrown if the service is unable to read
-     * the request for some reason.
-     */
-    @Override
-    protected void doPost(final HttpServletRequest request,
-                          final HttpServletResponse response)
-        throws ServletException, IOException {
-
-        // Open a SQL connection and create the user and log them in.
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+                  throws ServletException, IOException {
+        // set the attribute in the request to access it on the JSP
+        final ArrayList<User> users = new ArrayList<User>();
+        final ArrayList<University> univs = new ArrayList<University>();
+        final ArrayList<Department> depts = new ArrayList<Department>();
+        final ArrayList<Course> courses = new ArrayList<Course>();
         SQLSource.interact(new SQLInteractionInterface<Integer>() {
 
                 /**
@@ -63,13 +76,6 @@ public final class UserServlet extends HttpServlet {
                 public Integer run(Connection connection)
                     throws ServiceException, SQLException {
 
-                    String[] usernames = {"hschmack", "person1",
-                                          "SteveJobs27", "Drake"};
-                    String[] passwords = {"password1", "pass_word2", 
-                                          "PassWord3", "P455W0RD4"};
-                    String[] emails    = {"hayden@case.edu", "generic@gmail.com",
-                                          "jobs@apple.com", "hotlinebling@yahoo.com"};
-
                     // Create the test users
                     for (int i = 0; i < usernames.length; i++){
                         User user = User.create(connection,
@@ -77,16 +83,57 @@ public final class UserServlet extends HttpServlet {
                                             passwords[i],
                                             emails[i]);
 
+                        users.add(user);
+                    }
+                    // Create the test Universities
+                    for (int i = 0; i < univ_name.length; i++){
+                        University univ = University.create(connection,
+                                            univ_name[i],
+                                            univ_acron[i],
+                                            univ_city[i],
+                                            univ_state[i],
+                                            "USA");
+
+                        univs.add(univ);
+                    }
+                    //create test Departments
+                    for (int i = 0; i < dept_names.length; i++){
+                        //fill out 2 departments for two universities for testing purposes
+                        University univ = univs.get(i%2);
+
+                        Department dept = Department.create(connection,
+                                            univ,
+                                            dept_names[i],
+                                            dept_acro[i]);
+
+                        depts.add(dept);
                     }
 
+                    //create test Courses
+                    for (int i = 0; i < course_nums.length; i++){
+                        //fill out 2 departments for two universities for testing purposes
+                        Department dept;
+                        if (i < 3) {
+                            dept = depts.get(0); //CWRU EECS
+                        } else {
+                            dept = depts.get(2); //CWRU PHIL
+                        }
 
-                    
+                        Course course = Course.create(connection,
+                                            dept,
+                                            course_nums[i]);
+
+                        courses.add(course);
+                    }
                     // Have to return something.
                     return 0;
                 }
             });
-
-        // Redirect to profile page.
-        response.sendRedirect("/");
+        request.setAttribute("users", users);
+        request.setAttribute("univs", univs);
+        request.setAttribute("depts", depts);
+        request.setAttribute("courses", courses);
+        RequestDispatcher rd = getServletContext().getRequestDispatcher("/populatedb.jsp");
+        rd.forward(request, response);
     }
 }
--- service/src/main/webapp/populatedb.jsp
+++ service/src/main/webapp/populatedb.jsp
@@ -1,35 +1,86 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8" import="java.util.*"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" import="java.util.*"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <html>
     <head>
         <title>Populate Database</title>
         <link href="${pageContext.request.contextPath}/assets/css/base.css" rel="stylesheet" type="text/css">
     </head>
-	<body class="four-column">
+	<body>
         <jsp:include page="/header.jsp"/>
-        <div class="column-beta">
-            <h1>Populate the database with the following information</h1>
-            <h2>User Table</h2>
-            <table class="db-table">
-				<caption>User Table</caption>
-				<tr>
-					<th>Username</th>
-					<th>Password</th>
-					<th>Email</th>
-				</tr>
-				<tr>
-					<td>January</td>
-					<td>$100</td>
-					<td></td>
-				</tr>
-				<tr>
-					<td>February</td>
-					<td>$50</td>
-				</tr>
-			</table>
-            <form class="form-default" action="/servlet/populate" method="post" enctype="application/x-www-form-urlencoded">
-                    <button type="submit" class="button-large" id="submit" name="submit">Populate Database</button>
-            </form>
+        <h1>Database is populated  with the following information</h1>
+        <div>
+            <table>
+                <caption>User Table Additions</caption>
+                <tr>
+                    <th>Username</th>
+                    <th>Email</th>
+                </tr>
+                <c:forEach var="element" items="${users}">
+                    <tr>
+                        <td>${element.getUsername()}</td>
+                        <td>${element.getEmail()}</td>
+                    </tr>
+                </c:forEach>
+            </table>
+        </div>
+        <br></br>
+        <div>
+            <table>
+                <caption>University Table Additions</caption>
+                <tr>
+                    <th>Long Name</th>
+                    <th>Acronym</th>
+                    <th>City</th>
+                    <th>State</th>
+                    <th>Country</th>
+                </tr>
+                <c:forEach var="element" items="${univs}">
+                    <tr>
+                        <td>${element.getlongName()}</td>
+                        <td>${element.getAcronym()}</td>
+                        <td>${element.getCity()}</td>
+                        <td>${element.getState()}</td>
+                        <td>${element.getCountry()}</td>
+                    </tr>
+                </c:forEach>
+            </table>
+        </div>
+        <br></br>
+        <div>
+            <table>
+                <caption>Department Table Additions</caption>
+                <tr>
+                    <th>University</th>
+                    <th>Department Name</th>
+                    <th>Acronym</th>
+                </tr>
+                <c:forEach var="element" items="${depts}">
+                    <tr>
+                        <td>${element.getUniversity().getlongName()}</td>
+                        <td>${element.getDeptName()}</td>
+                        <td>${element.getAcronym()}</td>
+                    </tr>
+                </c:forEach>
+            </table>
+        </div>
+        <br></br>
+        <div>
+            <p>Note the University is not represented in the Course table, just included here for clarity</p>
+            <table>
+                <caption>Course Table Additions</caption>
+                <tr>
+                    <th>University</th>
+                    <th>Dept</th>
+                    <th>Number</th>
+                </tr>
+                <c:forEach var="element" items="${courses}">
+                    <tr>
+                        <td>${element.getDepartment().getUniversity().getlongName()}</td>
+                        <td>${element.getDepartment().getAcronym()}</td>
+                        <td>${element.getCourseNum()}</td>
+                    </tr>
+                </c:forEach>
+            </table>
         </div>
     </body>
 </html>
