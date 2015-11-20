93ca5e62ce48db0ad002a146b4f6607033c8b5ac
Christian Gunderman <gundermanc@gmail.com>
2015-11-20 13:12:52 -0500

    Commented UnivServlet.

--- service/src/main/java/com/fecklessweasel/service/UniversityServlet.java
+++ service/src/main/java/com/fecklessweasel/service/UniversityServlet.java
@@ -13,49 +13,52 @@ import com.fecklessweasel.service.datatier.SQLSource;
 import com.fecklessweasel.service.datatier.SQLInteractionInterface;
 import com.fecklessweasel.service.objectmodel.*;
 
+/**
+ * Servlet that handles updates to universities.
+ * @author Anjana Rao
+ */
 @WebServlet("/servlet/university")
 public final class UniversityServlet extends HttpServlet {
 
     /**
-     * Handles HTTP requests
+     * Handles HTTP POST requests
      *
      * @param request  to create a university
      * @param response redirects to university/index.jsp
-     * @throws ServletException
-     * @throws IOException
+     * @throws ServletException When any type of error occurs.
+     * @throws IOException If the servlet is unable to read the request.
      */
-
-    int univID;
-
     @Override
     protected void doPost(final HttpServletRequest request,
                           final HttpServletResponse response)
             throws ServletException, IOException {
 
-        SQLSource.interact(new SQLInteractionInterface<Integer>() {
-                               @Override
-                               public Integer run(Connection connection)
-                                       throws ServiceException, SQLException {
-                                   String longName = request.getParameter("longName");
-                                   String acronym = request.getParameter("acronym");
-                                   String city = request.getParameter("city");
-                                   String state = request.getParameter("state");
-                                   String country = request.getParameter("country");
+        int univID = SQLSource.interact(new SQLInteractionInterface<Integer>() {
+                @Override
+                public Integer run(Connection connection)
+                    throws ServiceException, SQLException {
+                    String longName = request.getParameter("longName");
+                    String acronym = request.getParameter("acronym");
+                    String city = request.getParameter("city");
+                    String state = request.getParameter("state");
+                    String country = request.getParameter("country");
 
-                                   //checks to null values
-                                   if (longName == null || acronym == null || city == null || state == null || country == null) {
-                                       throw new ServiceException(ServiceStatus.MALFORMED_REQUEST);
-                                   }
+                    // Checks for missing values
+                    if (longName == null ||
+                        acronym == null ||
+                        city == null ||
+                        state == null ||
+                        country == null) {
+                        throw new ServiceException(ServiceStatus.MALFORMED_REQUEST);
+                    }
 
-                                   // Create university
-                                   University university = University.create(connection, longName, acronym, city, state, country);
-                                   univID = university.getID();
-                                   // return int value
-                                   return 0;
-                               }
-                           }
+                    // Create university
+                    University university = University.create(connection, longName, acronym, city, state, country);
 
-        );
+                    // Get the ID of the new university.
+                    return university.getID();
+                }
+            });
 
         // Redirect to homepage.
         response.sendRedirect("/university/index.jsp?uid=" + univID);
