cff1130c446c5a823b32fd5bd0fee8daffcc1ad5
Elliot <ere16@case.edu>
2015-11-30 11:04:15 -0500

    removed tests

--- service/src/test/java/com/fecklessweasel/service/objectmodel/test_Comment.java
+++ service/src/test/java/com/fecklessweasel/service/objectmodel/test_Comment.java
@@ -16,40 +16,4 @@ import org.junit.runners.JUnit4;
 
 public class test_Comment {
 
-    private Connection mockConnection;
-    private User mockUser;
-    private FileMetadata mockFile;
-
-    @Before
-    public void setup() {
-        this.mockConnection = mock(Connection.class);
-        this.mockUser = mock(User.class);
-        this.mockFile = mock(FileMetadata.class);
-    }
-
-    @Test(expected = ServiceException.class)
-    public void test_create_nullConnection() throws Exception {
-        Comment.Create(null, this.mockUser, this.mockFile, "hello");
-    }
-
-    @Test(expected = ServiceException.class)
-    public void test_create_nullUser() throws Exception {
-        Comment.Create(this.mockConnection, null, this.mockFile, "hello");
-    }
-    
-    @Test(expected = ServiceException.class)
-    public void test_create_nullFile() throws Exception {
-        Comment.Create(this.mockConnection, this.mockUser, null, "hello");
-    }
-    
-    @Test(expected = ServiceException.class)
-    public void test_create_nullText() throws Exception {
-        Comment.Create(this.mockConnection, this.mockUser, this.mockFile, null);
-    }
-    
-    @Test(expected = ServiceException.class)
-    public void test_create_emptyText() throws Exception {
-        Comment.Create(this.mockConnection, this.mockUser, this.mockFile, "");
-    }
-
 }
--- service/src/test/java/com/fecklessweasel/service/objectmodel/test_Rating.java
+++ service/src/test/java/com/fecklessweasel/service/objectmodel/test_Rating.java
@@ -16,35 +16,5 @@ import org.junit.runners.JUnit4;
 
 public class test_Rating {
 
-    private Connection mockConnection;
-    private User mockUser;
-    private FileMetadata mockFile;
-
-    @Before
-    public void setup() {
-        this.mockConnection = mock(Connection.class);
-        this.mockUser = mock(User.class);
-        this.mockFile = mock(FileMetadata.class);
-    }
-
-    @Test(expected = ServiceException.class)
-    public void test_create_nullConnection() throws Exception {
-        Rating.Create(null, this.mockUser, this.mockFile, 3);
-    }
-    
-    @Test(expected = ServiceException.class)
-    public void test_create_nullUser() throws Exception {
-        Rating.Create(this.mockConnection, null, this.mockFile, 3);
-    }
-    
-    @Test(expected = ServiceException.class)
-    public void test_create_nullFile() throws Exception {
-        Rating.Create(this.mockConnection, this.mockUser, null, 3);
-    }
-    
-    @Test(expected = ServiceException.class)
-    public void test_create_negative_rating() throws Exception {
-        Rating.Create(this.mockConnection, this.mockUser, this.mockFile, -3);
-    }
 
 }
